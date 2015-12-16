d6bb1b0e4b1f14ed847679192c272b7446e3426e
De Olagundoye <de.olagundoye@gmail.com>
2015-12-16 16:16:54 -0500

    ok

--- quiz_app/db/migrate/20151216203221_create_students.rb
+++ quiz_app/db/migrate/20151216203221_create_students.rb
@@ -1,7 +1,8 @@
 class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
-      t.string :name
+      t.string :last_name
+      t.string :first_name
       t.string :email
       t.string :password_digest 
       t.references :cohort
