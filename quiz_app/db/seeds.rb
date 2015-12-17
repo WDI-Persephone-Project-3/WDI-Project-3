f34e2069dfaed89ec0e65c0470c30fb314e7bc22
De Olagundoye <de.olagundoye@gmail.com>
2015-12-17 17:55:15 -0500

    adds more seeds for testing purposes

--- quiz_app/db/seeds.rb
+++ quiz_app/db/seeds.rb
@@ -20,3 +20,29 @@ s3 = Student.create({last_name: "BeGood", first_name: "Johnny", email: "goodygoo
 s4 = Student.create({last_name: "Stallone", first_name: "Sylvester", email: "rocky@gmail.com", password: "123", password_confirmation: "123", cohort: c4})
 
 s5 = Student.create({last_name: "BeGood", first_name: "Johnny", email: "goodygoody@gmail.com", password: "123", password_confirmation: "123", cohort: c5})
+
+q1 = Question.create({content: "1 + 1"})
+q2 = Question.create({content: "1 + 2"})
+q3 = Question.create({content: "1 + 3"})
+
+a1 = Answer.create({choice: "1", question: q1, is_correct: false})
+a2 = Answer.create({choice: "2", question: q1, is_correct: true})
+a3 = Answer.create({choice: "3", question: q1, is_correct: false})
+a4 = Answer.create({choice: "4", question: q1, is_correct: false})
+
+
+a5 = Answer.create({choice: "1", question: q2, is_correct: false})
+a6 = Answer.create({choice: "2", question: q2, is_correct: false})
+a7 = Answer.create({choice: "3", question: q2, is_correct: true})
+a8 = Answer.create({choice: "4", question: q2, is_correct: false})
+
+a9 = Answer.create({choice: "1", question: q3, is_correct: false})
+a10 = Answer.create({choice: "2", question: q3, is_correct: false})
+a11 = Answer.create({choice: "3", question: q3, is_correct: false})
+a12 = Answer.create({choice: "4", question: q3, is_correct: true})
+
+test_quiz = Quiz.create({test_day: Date.today, instructor: Instructor.first})
+
+test_quiz.questions.push(q1)
+test_quiz.questions.push(q2)
+test_quiz.questions.push(q3)
