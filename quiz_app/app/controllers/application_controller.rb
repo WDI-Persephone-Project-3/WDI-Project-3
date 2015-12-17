f83fb9dc506ddafb00d1339e604fb5cf9ea4f128
Sergey Kolchinskiy <sergey.kolchinskiy@gmail.com>
2015-12-17 10:38:18 -0500

    added cohorts to routes, current_user helper_method to application_controller

--- quiz_app/app/controllers/application_controller.rb
+++ quiz_app/app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= Student.find(session[:user_id]) if session[:user_class] == "Student"
+    @current_user ||= Instructor.find(session[:user_id]) if session[:user_class] == "Instructor"
+  end
+
+  helper_method :current_user
+
 end
