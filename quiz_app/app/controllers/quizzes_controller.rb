0b0c2a6c8503a880bfaa20aef52bdc0d3cbbdab6
Ona Boseroy <o.boseroy@gmail.com>
2015-12-17 12:31:22 -0500

    starting quiz views

--- quiz_app/app/controllers/quizzes_controller.rb
+++ quiz_app/app/controllers/quizzes_controller.rb
@@ -12,7 +12,17 @@ class QuizzesController < ApplicationController
 
   # POST '/quizzes'
   def create
-    @
+    @quiz = Quiz.new(test_day: params[:test_day], user: current_user)
+    if @quiz.save
+      redirect_to '/quizzes'
+    else
+      render :new
+    end
+  end
+
+  # GET '/quizzes/1'
+  def show
+    @quiz = Quiz.find(params[:id])
   end
 
 
