f6ef56997cd92871a82e2bf343c16ae32a6ef577
Sergey Kolchinskiy <sergey.kolchinskiy@gmail.com>
2015-12-17 10:29:12 -0500

    initial commit

--- quiz_app/db/seeds.rb
+++ quiz_app/db/seeds.rb
@@ -1,37 +1,22 @@
-
-s1 = Student.create({last_name: "Wayne", first_name: "John", email: "johnwayne@gmail.com", password: "123", password_confirmation: "123"})
-
-s2 = Student.create({last_name: "Kent", first_name: "Clarke", email: "notsuperman@gmail.com", password: "123", password_confirmation: "123"})
-
-s3 = Student.create({last_name: "BeGood", first_name: "Johnny", email: "goodygoody@gmail.com", password: "123", password_confirmation: "123"})
-
-s4 = Student.create({last_name: "Stallone", first_name: "Sylvester", email: "rocky@gmail.com", password: "123", password_confirmation: "123"})
-
-s5 = Student.create({last_name: "BeGood", first_name: "Johnny", email: "goodygoody@gmail.com", password: "123", password_confirmation: "123"})
-
-c1 = Cohort.create({name: "Wolverine"})
-c2 = Cohort.create({name: "Prof X"})
-c3 = Cohort.create({name: "Storm"})
-c4 = Cohort.create({name: "Wonder Woman"})
-c5 = Cohort.create({name: "Cat Woman"})
-
-
-s1.cohort.push(c1)
-s2.cohort.push(c2)
-s3.cohort.push(c3)
-s4.cohort.push(c4)
-s5.cohort.push(c5)
-
-
 i1 = Instructor.create({last_name: "Pak", first_name: "Suhyun", email: "suhyun@quizapp.com", password: "123", password_confirmation: "123"})
 i2 = Instructor.create({last_name: "Olagundoye", first_name: "De", email: "de@quizapp.com", password: "123", password_confirmation: "123"})
 i3 = Instructor.create({last_name: "Kolchinskiy", first_name: "Sergey", email: "sergey@quizapp.com", password: "123", password_confirmation: "123"})
 i4 = Instructor.create({last_name: "Boseroy", first_name: "Ona", email: "ona@quizapp.com", password: "123", password_confirmation: "123"})
 i5 = Instructor.create({last_name: "Gandikota", first_name: "Anusha", email: "anusha@quizapp.com", password: "123", password_confirmation: "123"})
 
-c1.instructor.push(i1)
-c2.instructor.push(i3)
-c3.instructor.push(i2)
-c4.instructor.push(i4)
-c5.instructor.push(i5)
 
+c1 = Cohort.create({name: "Wolverine", instructor: i1})
+c2 = Cohort.create({name: "Prof X", instructor: i3})
+c3 = Cohort.create({name: "Storm", instructor: i2})
+c4 = Cohort.create({name: "Wonder Woman", instructor: i4})
+c5 = Cohort.create({name: "Cat Woman", instructor: i5})
+
+s1 = Student.create({last_name: "Wayne", first_name: "John", email: "johnwayne@gmail.com", password: "123", password_confirmation: "123", cohort_id: c1})
+
+s2 = Student.create({last_name: "Kent", first_name: "Clarke", email: "notsuperman@gmail.com", password: "123", password_confirmation: "123", cohort_id: c2})
+
+s3 = Student.create({last_name: "BeGood", first_name: "Johnny", email: "goodygoody@gmail.com", password: "123", password_confirmation: "123", cohort_id: c3})
+
+s4 = Student.create({last_name: "Stallone", first_name: "Sylvester", email: "rocky@gmail.com", password: "123", password_confirmation: "123", cohort_id: c4})
+
+s5 = Student.create({last_name: "BeGood", first_name: "Johnny", email: "goodygoody@gmail.com", password: "123", password_confirmation: "123", cohort_id: c5})
