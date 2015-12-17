f696a1254e42b5548c3e67632942e4391715a97d
Suhyun Pak <searchforac@gmail.com>
2015-12-17 15:52:22 -0500

    stuff

--- quiz_app/app/controllers/sessions_controller.rb
+++ quiz_app/app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@ class SessionsController < ApplicationController
 
 	def new
     if session[:user_class] == "Student"
-      redirect_to 'students/dash'
+      redirect_to '/students/dash'
     elsif session[:user_class] == "Instructor"
-      redirect_to 'instructors/dash'
+      redirect_to '/instructors/dash'
     else
       render :new
     end
