963d7228cbd67bdcbeea5cf82e810051f5bde200
Anusha Gandikota <nush1113@gmail.com>
2015-12-17 11:56:43 -0500

    adds method to question model and student controller

--- quiz_app/app/controllers/students_controller.rb
+++ quiz_app/app/controllers/students_controller.rb
@@ -1,2 +1,22 @@
 class StudentsController < ApplicationController
+
+  def show
+  	@student = Student.find(params[:id])
+  end
+
+  def edit
+  	render :edit 
+  end
+
+  def update
+  	@student = Student.find(params[:id])
+  	@student.update(student_params)
+  end
+  
+  private 
+
+  def student_params
+  	params.require(:student).permit(:email, :password, :password_confirmation)
+  end
+
 end
