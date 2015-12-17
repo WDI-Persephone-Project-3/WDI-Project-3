9ab37c21add64c7e8565f8a1b9aa1423c4dd2fa9
Ona Boseroy <o.boseroy@gmail.com>
2015-12-17 11:58:35 -0500

    starts quiz controller

--- quiz_app/app/controllers/quizzes_controller.rb
+++ quiz_app/app/controllers/quizzes_controller.rb
@@ -1,2 +1,19 @@
 class QuizzesController < ApplicationController
+
+  # GET '/quizzes'
+  def index
+    @quizzes = Quiz.all
+  end
+
+  # GET '/quizzes/new'
+  def new
+    @quiz = Quiz.new
+  end
+
+  # POST '/quizzes'
+  def create
+    @
+  end
+
+
 end
