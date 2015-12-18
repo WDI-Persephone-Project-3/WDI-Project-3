e8871e7bb411573ab080ebb24ae2cd40e00c9261
De Olagundoye <de.olagundoye@gmail.com>
2015-12-17 22:52:25 -0500

    complete instructors controller and views

--- quiz_app/app/controllers/instructors_controller.rb
+++ quiz_app/app/controllers/instructors_controller.rb
@@ -1,27 +1,30 @@
 class InstructorsController < ApplicationController
 
   def dash
+    @instructor = Instructor.find(session[:user_id])
   end
 
   def show
     @instructor = Instructor.find(params[:id])
   end
 
-  def edit
-    render :edit 
+  def edit  
+    @instructor = Instructor.find(params[:id])
   end
 
   def update
     @instructor = Instructor.find(params[:id])
     @instructor.update(instructor_params)
+    redirect_to "/"
   end
-  
-  def cohort
+
+  def new
   end
 
   private 
 
   def instructor_params
+    params.require(:instructor).permit(:last_name, :first_name, :email_address)
   end
 
 end
